179478b7d83a8466f129bae4b9dff80aef19cdbb
Haroon <95302904+HaroonRashid123@users.noreply.github.com>
2024-10-20 18:37:45 -0400

    assignment2

--- assignment2.py
+++ assignment2.py
@@ -1,33 +1,33 @@
 import csv
 import numpy as np
 import math
+
 def naive_bayes_classifier(dataset_filepath, patient_measurements):
-  
   data = mean_and_stand(dataset_filepath)
-  temperature = patient_measurements[0]
-  heart_rate = patient_measurements[1]
-  temp_density_healthy = prob_den_func(temperature, data['healthy_temp_mean'], data['healthy_temp_std'])
-  heart_density_healthy = prob_den_func(heart_rate, data['healthy_heart_mean'], data['healthy_heart_std'])
-  temp_density_disease = prob_den_func(temperature, data['diseased_temp_mean'], data['diseased_temp_std'])
-  heart_density_disease = prob_den_func(heart_rate, data['diseased_heart_mean'], data['diseased_heart_std'])
-    
-  health =  temp_density_healthy * heart_density_healthy
-  disease = temp_density_disease * heart_density_disease
+  #Using PDF function to calculate prob density
+  temp_density_healthy = prob_den_func(patient_measurements[0], data['healthy_temp_mean'], data['healthy_temp_std'])
+  heart_density_healthy = prob_den_func(patient_measurements[1], data['healthy_heart_mean'], data['healthy_heart_std'])
+  temp_density_disease = prob_den_func(patient_measurements[0], data['diseased_temp_mean'], data['diseased_temp_std'])
+  heart_density_disease = prob_den_func(patient_measurements[1], data['diseased_heart_mean'], data['diseased_heart_std'])
+
+  health =  temp_density_healthy * heart_density_healthy * data['prior_health'] 
+  disease = temp_density_disease * heart_density_disease * data['prior_disease']
   total = health + disease
+
+  #Normalize the probability
   normal_health = health / total
   normal_disease =  disease / total
-    
+  # Calculate which normal is greater and return the value of which is    
   if normal_health > normal_disease:
         most_likely_class = 'healthy'
   else:
         most_likely_class = 'diseased'
-    
-  class_probabilities = [normal_health, normal_disease]
-  return most_likely_class, class_probabilities
-
+        
+  return most_likely_class, [normal_health, normal_disease]
 
 
 def csv_format(filename):
+    #csv format to get a 2d list from the csv file
     grid = []
     with open(filename, 'r') as file:
         csv_reader = csv.reader(file)
@@ -38,13 +38,14 @@ def csv_format(filename):
 
 def prob_den_func(x, mean, std):
     
-    #expo = math.exp(-((x - mean) ** 2) / (2 * std ** 2))
+    # Probability densisty function functions for temperature and heart rate for healthy
+    # patients and diseased patients from an existing dataset of patients
 
     exponent_value = -0.5 * (( (x- mean) / std )) **2
-    bottom_value = (1 / (std * math.sqrt(2 * math.pi))) * math.e
-    result = bottom_value * math.e * math.exp(exponent_value)
+    bottom_value = (1 / (std * math.sqrt(2 * math.pi))) 
+    result = bottom_value *  math.exp(exponent_value)
 
-    return result
+    return result 
   
 def mean_and_stand(grid):
   grid = csv_format(grid)
@@ -52,35 +53,41 @@ def mean_and_stand(grid):
   healthy_heart_list = []
   diseased_temp_list = []
   diseased_heart_list = []
-
-
+  num_of_healthy = 0
+  num_of_disease = 0
+  data = {}
+  #Loop through the 2d array to add the healthy and disease columns while also incrementing the number of patients for each
   for i in range(len(grid)):
-    
+      
       if 'healthy' in grid[i]:
             healthy_temp_list.append(float(grid[i][1]))
             healthy_heart_list.append(float(grid[i][2]))
+            num_of_healthy +=1
       else:
             diseased_temp_list.append(float(grid[i][1]))
             diseased_heart_list.append(float(grid[i][2]))
-  
-  
+            num_of_disease += 1
+  # Get average of total patients divided by number of pateince between health and disease
+  prior_health = num_of_healthy / (i +1)
+  print(prior_health)
+  prior_disease = num_of_disease / (i + 1)
    
   healthy_temp_array = np.array(healthy_temp_list)
   healthy_heart_array = np.array(healthy_heart_list)
   diseased_temp_array = np.array(diseased_temp_list)
   diseased_heart_array = np.array(diseased_heart_list)
 
-  
+  #Calculate mean and standard deviation of tempature and heartrate
   healthy_temp_mean = np.mean(healthy_temp_array)
   healthy_heart_mean = np.mean(healthy_heart_array)
-  healthy_temp_std = np.std(healthy_temp_array, ddof=0)  
-  healthy_heart_std = np.std(healthy_heart_array, ddof=0)
+  healthy_temp_std = np.std(healthy_temp_array, ddof=1)  
+  healthy_heart_std = np.std(healthy_heart_array, ddof=1)
 
   diseased_temp_mean = np.mean(diseased_temp_array)
   diseased_heart_mean = np.mean(diseased_heart_array)
-  diseased_temp_std = np.std(diseased_temp_array, ddof=0)
-  diseased_heart_std = np.std(diseased_heart_array, ddof=0)
-
+  diseased_temp_std = np.std(diseased_temp_array, ddof=1)
+  diseased_heart_std = np.std(diseased_heart_array, ddof=1)
+  #Put all this data into a dictionary so i can use it in the bayes classifier function
   data = {
         "healthy_temp_mean": healthy_temp_mean,
         "healthy_heart_mean": healthy_heart_mean,
@@ -89,20 +96,14 @@ def mean_and_stand(grid):
         "diseased_temp_mean": diseased_temp_mean,
         "diseased_heart_mean": diseased_heart_mean,
         "diseased_temp_std": diseased_temp_std,
-        "diseased_heart_std": diseased_heart_std
+        "diseased_heart_std": diseased_heart_std,
+        "prior_health": prior_health,
+        "prior_disease": prior_disease
     }
   return data
 
-
-
-  
-  
-        
-         
-         
-
 def main():
-  dataset_filepath = r'C:\Users\haroo\Desktop\AIA2\Examples\Example1\dataset.csv'
+  dataset_filepath = r'C:\Users\haroo\Desktop\AIA2\Examples\Example0\dataset.csv'
   patient_measurements = [37.6, 80]  
   print(naive_bayes_classifier(dataset_filepath, patient_measurements))
  
